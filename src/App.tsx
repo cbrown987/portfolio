73d025c5ad05cebffd29bdec84fc7927f94c5f15
Cbrown <cooper.brown@drake.edu>
2024-04-17 11:34:00 -0500

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Analytics } from "@vercel/analytics/react"
+
 
 import './App.css';
 import {Header} from "./components/Header/Header";
@@ -18,35 +20,39 @@ function App() {
     };
 
     return (
-        <div className="App">
-            <NavBar
-                dropdownButtonText={data.navbar.projects[language]}
-                dropdownProgramingText={data.navbar.projects_programing[language]}
-                dropdownMLText={data.navbar.projects_ml[language]}
-                dropdownContactText={data.navbar.contact[language]}
-                languageSwitchText={data.navbar.switchLang[language]}
-                toggleLanguage={toggleLanguage}
-            />
-            <Header/>
-            <Project
-                groupName={"programing"}
-                projectList={data.projects.programing}
-                language={language}
-            />
-            <hr />
-            <Project
-                groupName={"machine-learning"}
-                projectList={data.projects.ml}
-                language={language} />
-            <Contact
-                socialsText={data.contact.socials[language]}
-                emailText={data.contact.email[language]}
-                resumeText={data.contact.resume[language]}
-                downloadText={data.contact.downloadResume[language]}
+        <>
+            <div className="App">
+                <NavBar
+                    dropdownButtonText={data.navbar.projects[language]}
+                    dropdownProgramingText={data.navbar.projects_programing[language]}
+                    dropdownMLText={data.navbar.projects_ml[language]}
+                    dropdownContactText={data.navbar.contact[language]}
+                    languageSwitchText={data.navbar.switchLang[language]}
+                    toggleLanguage={toggleLanguage}
+                />
+                <Header/>
+                <Analytics />
+                <Project
+                    groupName={"programing"}
+                    projectList={data.projects.programing}
+                    language={language}
+                />
+                <hr />
+                <Project
+                    groupName={"machine-learning"}
+                    projectList={data.projects.ml}
+                    language={language} />
+                <Contact
+                    socialsText={data.contact.socials[language]}
+                    emailText={data.contact.email[language]}
+                    resumeText={data.contact.resume[language]}
+                    downloadText={data.contact.downloadResume[language]}
 
-            />
-        </div>
+                />
+            </div>
+        </>
     );
+
 }
 
 export default App;
